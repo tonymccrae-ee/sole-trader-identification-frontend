b016ecbd2e29a52f94c1150828d0814cbe6fd19e
Daveo-G <Daveo-G@users.noreply.github.com>
2020-07-08 15:59:11 +0100

    SAR-5772-V2 - Added validation to nino page

---
+++ app/uk/gov/hmrc/soletraderidentificationfrontend/forms/CaptureNinoForm.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.soletraderidentificationfrontend.forms
+
+import play.api.data.Form
+import play.api.data.Forms._
+
+import scala.util.matching.Regex
+
+object CaptureNinoForm {
+  val ninoErrorKey: String = "enter-nino.invalid.format.error"
+  val ninoRegex: Regex = "^([ACEHJLMOPRSWXY][A-CEGHJ-NPR-TW-Z]|B[A-CEHJ-NPR-TW-Z]|G[ACEGHJ-NPR-TW-Z]|[KT][A-CEGHJ-MPR-TW-Z]|N[A-CEGHJL-NPR-SW-Z]|Z[A-CEGHJ-NPR-TW-Y])[0-9]{6}[A-D ]?$".r
+
+  val form: Form[String] =
+    Form(
+      "nino" -> text.verifying(ninoErrorKey, _.matches(ninoRegex.regex))
+    )
+
+}
